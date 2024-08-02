c71b3c4fe2000182a129dd09b4afee182fdefc90
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-02 23:33:53 +0600

    add the flowbite

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,21 +1,18 @@
 
 import Footer from "../Footer/Footer";
-
-
-
+import NavbarTop from "../Navbar/NavbarTop";
 const Main = () => {
     return (
         <>
-
-
-            <Footer></Footer>
-
-
-
-
-
-
-
+            <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+                <div className="w-full h-full fixed top-0 -z-10">
+                    <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+                </div>
+                <div className="max-w-screen-xl mx-auto">
+                    <NavbarTop></NavbarTop>
+                    <Footer></Footer>
+                </div>
+            </div>
         </>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,13 @@ const flowbite = require("flowbite-react/tailwind");
 module.exports = {
   content: [
     // ...
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
     flowbite.content(),
   ],
+  theme: {
+    extend: {},
+  },
   plugins: [
     // ...
     flowbite.plugin(),
